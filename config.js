141eaf06a444166b14d2ef9611ce04b9c063e9c9
PanDaoxi <2060642520@qq.com>
2025-04-12 22:24:13 +0800

    init

---
+++ config.js
@@ -0,0 +1,1 @@
+window.Config={SiteName:'PanDaoxi 的站点监控',ApiKeys:['ur2898477-7b656046e7c5fde8dfa7c660',],CountDays:90,ShowLink:true,Navi:[{text:'Homepage',url:'https://daoxi365.top/'},{text:'GitHub',url:'https://github.com/pandaoxi'},{text:'Blog',url:'https://blog.daoxi365.top/'},],};
