20b6482df260ff91eec3b47fe3f03e7358ddd847
PanDaoxi <2060642520@qq.com>
2025-05-25 22:42:44 +0800

    更新名称

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
-window.Config={SiteName:'PanDaoxi 的站点监控',ApiKeys:['ur2898477-7b656046e7c5fde8dfa7c660',],CountDays:90,ShowLink:true,Navi:[{text:'Homepage',url:'https://daoxi365.top/'},{text:'GitHub',url:'https://github.com/pandaoxi'},{text:'Blog',url:'https://blog.daoxi365.top/'},],};
+window.Config = {
+	SiteName: "PanDaoxi 的站点监控",
+	ApiKeys: ["ur2898477-7b656046e7c5fde8dfa7c660"],
+	CountDays: 90,
+	ShowLink: true,
+	Navi: [
+		{ text: "Homepage", url: "https://www.daoxi365.top/" },
+		{ text: "GitHub", url: "https://github.com/pandaoxi" },
+		{ text: "Blog", url: "https://blog.daoxi365.top/" },
+	],
+};
